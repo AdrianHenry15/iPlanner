db336ad5d57545cc34fd264ce91259f17fc0289d
Adrian <adrianhenry2115@gmail.com>
2022-01-27 18:26:47 -0500

    html update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let currentDay = document.getElementById('currentDay');
+var tasks = {};
+
+function getLiveDate() {
+    var today = new Date();
+    var date = today.toLocaleDateString("en-EN", { weekday: 'long' }) + ', ' + (today.getMonth() + 1) + '/' + today.getDate();
+
+    
+  $("#currentDay").append(date);
+}
+getLiveDate(); 
+
+
+
