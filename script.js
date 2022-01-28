07788102d691194ad52c9659dc48faa2d12e992f
Adrian <adrianhenry2115@gmail.com>
2022-01-27 19:00:25 -0500

    hour update and js start

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-var tasks = {};
+$(document).ready(function () {
+    $('.saveBtn').on('click', function () {
+        var value = $(this).siblings('.to-dos').val();
+        var time = $(this).parent().attr('id');
+        localStorage.setItem(time, value)
+    });
 
-function getLiveDate() {
-    var today = new Date();
-    var date = today.toLocaleDateString("en-EN", { weekday: 'long' }) + ', ' + (today.getMonth() + 1) + '/' + today.getDate();
+    function getLiveDate() {
+        var today = new Date();
+        var date = today.toLocaleDateString("en-EN", { weekday: 'long' }) + ', ' + (today.getMonth() + 1) + '/' + today.getDate();
 
-    
-  $("#currentDay").append(date);
-}
-getLiveDate(); 
+
+        $("#currentDay").append(date);
+    }
+    getLiveDate();
+
+    function getTime() {
+        
+    }
+    var interval = setInterval()
+
+});
+
+
+
+
+
 
 
 
