4600369aee5ae7ac3f8be3ad6ac8a5a153186cf3
Adrian <adrianhenry2115@gmail.com>
2022-01-28 16:53:56 -0500

    current hour bug fixed

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-    var currentHour = moment().format('LT');
+    //var currentHour = moment().format('LT');
+    let nowMoment = moment().format("MMMM Do YYYY")
+    let displayDate = document.getElementById("currentDay");
+    let currentHour = moment().format("HH")
 
 
     function getLiveDate() {
@@ -17,34 +20,35 @@ $(document).ready(function () {
         localStorage.clear();
     })
 
-    $(".time-block").each(function () {
-        var timeContainer = $(this).attr("id").split("-")[1]
+    $(".time-div").each(function () {
+        var timeDiv = $(this).attr("id").split("-")[1]
 
-        if (currentHour == timeContainer) {
+        if (currentHour == timeDiv) {
             $(this).addClass("present");
             $(this).children(".description").addClass("white-text");
-        } else if (currentHour < timeContainer) {
+        } else if (currentHour < timeDiv) {
             $(this).removeClass("present");
             $(this).addClass("future");
-        } else if (currentHour > timeContainer) {
+        } else if (currentHour > timeDiv) {
             $(this).removeClass("future");
             $(this).addClass("past");
         }
     })
-    $('.saveBtn').on('click', function () {
-        var value = $(this).siblings('.to-dos').val();
+    $('.saveBtn').on('click', function (event) {
+        event.preventDefault();
+        var value = $(this).siblings('.time-block').val();
         var time = $(this).parent().attr('id').split("-")[1];
         localStorage.setItem(time, value)
     });
-    $('#hour-9 .to-dos').val(localStorage.getItem("9"));
-    $('#hour-10 .to-dos').val(localStorage.getItem("10"));
-    $('#hour-11 .to-dos').val(localStorage.getItem("11"));
-    $('#hour-12 .to-dos').val(localStorage.getItem("12"));
-    $('#hour-13 .to-dos').val(localStorage.getItem("13"));
-    $('#hour-14 .to-dos').val(localStorage.getItem("14"));
-    $('#hour-15 .to-dos').val(localStorage.getItem("15"));
-    $('#hour-16 .to-dos').val(localStorage.getItem("16"));
-    $('#hour-17 .to-dos').val(localStorage.getItem("17"));
+    $('#hour-09 .time-block').val(localStorage.getItem("09"));
+    $('#hour-10 .time-block').val(localStorage.getItem("10"));
+    $('#hour-11 .time-block').val(localStorage.getItem("11"));
+    $('#hour-12 .time-block').val(localStorage.getItem("12"));
+    $('#hour-13 .time-block').val(localStorage.getItem("13"));
+    $('#hour-14 .time-block').val(localStorage.getItem("14"));
+    $('#hour-15 .time-block').val(localStorage.getItem("15"));
+    $('#hour-16 .time-block').val(localStorage.getItem("16"));
+    $('#hour-17 .time-block').val(localStorage.getItem("17"));
 
 
 
