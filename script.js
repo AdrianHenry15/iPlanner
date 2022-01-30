748ee0afc7e5ab8102fb4042b8765fbf7c1219e8
Adrian <adrianhenry2115@gmail.com>
2022-01-29 21:45:01 -0500

    Javascript comments

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// Lets the document know that this whole file will be in jQuery
 $(document).ready(function () {
-    //var currentHour = moment().format('LT');
+    // These variables use moment.js and are for the live date and times to be displayed and used for logic
     let nowMoment = moment().format("MMMM Do YYYY")
     let displayDate = document.getElementById("currentDay");
     let currentHour = moment().format("HH")
 
-
+    // This funtion displays the date in the header section of the webpage
     function getLiveDate() {
         var today = new Date();
         var date = today.toLocaleDateString("en-EN", { weekday: 'long' }) + ', ' + (today.getMonth() + 1) + '/' + today.getDate();
@@ -14,12 +15,16 @@ $(document).ready(function () {
     }
     getLiveDate();
 
+    // This funtion clears localstorage
     $("#clearFieldsBtn").click(function (event) {
         event.preventDefault;
         $("textarea").val("");
         localStorage.clear();
     })
 
+    // This function uses moment.js from the variables we placed in the top 
+    // of the document to display different colors to the sections 
+    // depending on if we are in the past, present or future.
     $(".time-div").each(function () {
         var timeDiv = $(this).attr("id").split("-")[1]
 
@@ -33,13 +38,16 @@ $(document).ready(function () {
             $(this).removeClass("future");
             $(this).addClass("past");
         }
-    })
+    });
+    // This function saves the user input into localstorage
     $('.saveBtn').on('click', function (event) {
         event.preventDefault();
         var value = $(this).siblings('.time-block').val();
         var time = $(this).parent().attr('id').split("-")[1];
         localStorage.setItem(time, value)
     });
+
+    // This allows the saved data from the user to persist on the page
     $('#hour-09 .time-block').val(localStorage.getItem("09"));
     $('#hour-10 .time-block').val(localStorage.getItem("10"));
     $('#hour-11 .time-block').val(localStorage.getItem("11"));
